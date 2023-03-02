6d42cdf6c2eb38b943d2319bafbab48adcfbc16d
victor.kapral <kapviktor@gmail.com>
2023-03-02 21:48:02 +0300

    finish before checking the homework

---
+++ yatube/posts/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from .models import Group
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context) 
+
+def groups(request):
+    template = 'posts/group_list.html'
+    return render(request, template)
