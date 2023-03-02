a6ee1f6cc98543ebd377ed3baaf2b316f5337906
victor.kapral <kapviktor@gmail.com>
2023-03-02 22:01:47 +0300

    fix some typos

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -4,24 +4,26 @@ from .models import Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('pk', 
-                    'text', 
-                    'pub_date', 
-                    'author', 
+    list_display = ('pk',
+                    'text',
+                    'pub_date',
+                    'author',
                     'group')
     list_editable = ('group',)
     search_fields = ('text',)
     list_filter = ('pub_date',)
     empty_value_display = '-пусто-'
 
+
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('pk', 
-                    'title', 
-                    'slug', 
+    list_display = ('pk',
+                    'title',
+                    'slug',
                     'description')
     search_fields = ('title',)
     list_filter = ('title',)
     empty_value_display = '-пусто-'
+
 
 # При регистрации модели Post источником конфигурации для неё назначаем
 # класс PostAdmin
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from .models import Group
 
+
 def index(request):
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
+
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
@@ -16,7 +18,8 @@ def group_posts(request, slug):
         'group': group,
         'posts': posts,
     }
-    return render(request, 'posts/group_list.html', context) 
+    return render(request, 'posts/group_list.html', context)
+
 
 def groups(request):
     template = 'posts/group_list.html'
